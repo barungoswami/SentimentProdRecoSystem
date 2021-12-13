f788a76737b3b66500e55beaa846c402bc8032df
Barun Kumar Goswami <42234575+barungoswami@users.noreply.github.com>
2021-12-13 18:15:56 +0530

    Update model.py

--- model.py
+++ model.py
@@ -28,21 +28,6 @@ class Recommendation:
         self.model = pickle.load(open('logistic_regression.pkl','rb'))
         self.raw_data = pd.read_csv("sample30.csv")
         self.data = pd.concat([self.raw_data[['id','name','brand','categories','manufacturer']],self.data], axis=1)
-        
-        
-    def getTopProducts(self, user):
-        items = self.user_final_rating.loc[user].sort_values(ascending=False)[0:20].index
-        tfs=pd.read_pickle('tfidf.pkl')
-        temp=self.data[self.data.id.isin(items)]
-        X = tfs.transform(temp['Review'].values.astype(str))
-        temp=temp[['id']]
-        temp['prediction'] = self.model.predict(X)
-        temp['prediction'] = temp['prediction'].map({'Postive':1,'Negative':0})
-        temp=temp.groupby('id').sum()
-        temp['positive_percent']=temp.apply(lambda x: x['prediction']/sum(x), axis=1)
-        final_list=temp.sort_values('positive_percent', ascending=False).iloc[:5,:].index
-        return self.data[self.data.id.isin(final_list)][['id', 'brand',
-                              'categories', 'manufacturer', 'name']].drop_duplicates().to_html(index=False)
 
     def getTopProductsNew(self, user):
         items = self.user_final_rating.loc[user].sort_values(ascending=False)[0:5].index
